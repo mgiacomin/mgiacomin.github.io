837e5a4ad06232d14beb2e9332bed4dc57d86c03
Maurizio Giacomin <maurizio.giacomin@unipd.it>
2025-12-13 17:59:43 +0100

    Create talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,57 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
+    "63rd Annual Meeting of the APS Division of Plasma Physics<br />Pittsburgh, Pennsylvania",
+    40.4406968,
+    -80.0025666
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "18th European Fusion Theory Conference<br />Ghent, Belgium",
+    51.0538286,
+    3.7250121
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "49th European Conference on Plasma Physics<br />Bordeaux, France",
+    44.841225,
+    -0.5800364
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "49th IOP Annual Plasma Physics Conference<br />Oxford, United Kingdom",
+    51.7520131,
+    -1.2578499
+  ],
+  [
+    "Plasma Edge Theory Workshop - 2021<br />Lausanne, Switzerland",
+    46.5218269,
+    6.6327025
+  ],
+  [
+    "25th Joint EU-US Transport Task Force Meeting<br />York, United Kingdom",
+    53.9656579,
+    -1.0743052
+  ],
+  [
+    "27th Joint EU-US Transport Task Force Meeting<br />Nancy, France",
+    48.6937223,
+    6.1834097
+  ],
+  [
+    "29th EU-US Transport Task Force Workshop<br />Budapest, Hungary",
+    47.4978789,
+    19.0402383
+  ],
+  [
+    "Joint Varenna-Lausanne International Workshop<br />Lausanne, Switzerland",
+    46.5218269,
+    6.6327025
+  ],
+  [
+    "Joint Varenna-Lausanne International Workshop<br />Varenna, Italy",
+    46.0099785,
+    9.2831593
+  ],
+  [
+    "15th Plasma Kinetics Working Meeting<br />Vienna, Austria",
+    48.2083537,
+    16.3725042
   ]
 ];
